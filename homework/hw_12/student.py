0c6fe12f3154403331aaaaf234e3ed25ef11dbf9
Yakovlev Andrew <drozy@yandex.ru>
2023-12-06 14:54:17 +0300

    12 homework in progress

--- homework/hw_12/student.py
+++ homework/hw_12/student.py
@@ -38,26 +38,34 @@ class Student:
     def __init__(self, name, subjects_file):
         """
         Конструктор класса. Принимает имя студента и файл с предметами и их результатами.
-        Инициализирует атрибуты name и subjects и вызывает метод load_subjects для загрузки предметов из файла.
+        Инициализирует атрибуты name и subjects и вызывает метод load_subjects
+        для загрузки предметов из файла.
         """
         self.name = name
-        self.subjects = self.load_subjects(subjects_file)
+        self.subjects = {}
+        self.load_subjects(subjects_file)
 
     def __setattr__(self, name, value):
         """
         Дескриптор, который проверяет установку атрибута name.
         Убеждается, что name начинается с заглавной буквы и состоит только из букв.
         """
-        if value.isalpha() and value[0].isupper():
-            self.__dict__[name] = value
+        alphabet = {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
+                    "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"}
+        if name == "name":
+            if not bool(alphabet.intersection(set(value.lower()))) or value[0].islower():
+                print("ФИО должно состоять только из букв и начинаться с заглавной буквы")
         else:
-            print("ФИО должно состоять только из букв и начинаться с заглавной буквы")
+            self.__dict__[name] = value
 
-    def __getattr__(self, name):
+    def __getattr__(self, name):    # TODO: что-то не так
         """
         Позволяет получать значения атрибутов предметов (оценок и результатов тестов) по их именам.
         """
-        pass
+
+        if name in self.subjects:
+            return self.subjects[name]
+        return f'Предмет {name} не найден'
 
     def __str__(self):
         """
@@ -66,10 +74,12 @@ class Student:
         Предметы: Математика, История
         """
         subjects = ""
-        for key in self.subjects.keys():
-            if subjects:
-                subjects = subjects + ", " + key
-        return f'Студент: {self.name}\nПредметы:{subjects}'
+        for subj in self.subjects:
+            if subjects != "":
+                subjects = subjects + ", " + subj
+            else:
+                subjects = subj
+        return f'Студент: {self.name}\nПредметы: {subjects}'
 
     def load_subjects(self, subjects_file):
         """
@@ -77,29 +87,69 @@ class Student:
         Использует модуль csv для чтения данных из файла и добавляет предметы в атрибут subjects.
         """
         with open(subjects_file, encoding="utf-8") as csv_f:
-            return dict(csv.reader(csv_f))
+            for subj in list(csv.reader(csv_f))[0]:
+                self.subjects[subj] = {'grades': [], 'test_scores': []}
 
     def add_grade(self, subject, grade):
         """
-        Добавляет оценку по заданному предмету. Убеждается, что оценка является целым числом от 2 до 5.
+        Добавляет оценку по заданному предмету.
+        Убеждается, что оценка является целым числом от 2 до 5.
         """
-        pass
+        if 2 <= grade <= 5 and isinstance(grade, int):
+            if subject in self.subjects:
+                self.subjects[subject]['grades'].append(grade)
+            else:
+                print(f"Предмет {subject} не найден")
+        else:
+            print("Оценка должна быть целым числом от 2 до 5")
 
     def add_test_score(self, subject, test_score):
         """
         Добавляет результат теста по заданному предмету.
         Убеждается, что результат теста является целым числом от 0 до 100.
         """
-        pass
+        if 0 <= test_score <= 100 and isinstance(test_score, int):
+            if subject in self.subjects:
+                self.subjects[subject]['test_scores'].append(test_score)
+            else:
+                print(f"Предмет {subject} не найден")
+        else:
+            print("Результат теста должен быть целым числом от 0 до 100")
 
     def get_average_test_score(self, subject):
         """
         Возвращает средний балл по тестам для заданного предмета.
         """
-        pass
+        if subject in self.subjects:
+            test_scores = self.subjects[subject]['test_scores']
+            return sum(test_scores) / len(test_scores)
+        raise ValueError(f"Предмет {subject} не найден")
 
     def get_average_grade(self):
         """
         Возвращает средний балл по всем предметам.
         """
-        pass
+        grades_count = 0
+        grades_sum = 0
+        for value in self.subjects.values():
+            grades_count += len(value['grades'])
+            grades_sum += sum(value['grades'])
+        return grades_sum / grades_count
+
+
+if __name__ == '__main__':
+    student = Student("Иван Иванов", "subjects.csv")
+
+    student.add_grade("Математика", 4)
+    student.add_test_score("Математика", 85)
+
+    student.add_grade("История", 5)
+    student.add_test_score("История", 92)
+
+    print(student)
+
+    average_grade = student.get_average_grade()
+    print(f"Средний балл: {average_grade}")
+
+    average_test_score = student.get_average_test_score("Математика")
+    print(f"Средний результат по тестам по математике: {average_test_score}")
