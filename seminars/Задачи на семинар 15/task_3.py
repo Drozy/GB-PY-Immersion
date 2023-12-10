889cc0cd3636e780da5c5edf030c6b6d37dc4b29
Yakovlev Andrew <drozy@yandex.ru>
2023-12-10 23:58:17 +0300

    15 seminar added some changes

--- seminars/Задачи на семинар 15/task_3.py
+++ seminars/Задачи на семинар 15/task_3.py
@@ -1,7 +1,7 @@
 """
 Задание №3
  Доработаем задачу 2.
- Сохраняйте в лог файл раздельно:
+ Сохраняйте в лог-файл раздельно:
 ○ уровень логирования,
 ○ дату события,
 ○ имя функции (не декоратора),
@@ -21,18 +21,21 @@ logging.basicConfig(filename='Log/log_3.log',
 logger = logging.getLogger(__name__)
 
 
-def decor(func):
+def decor_logger(func):
     def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        logger.info(
-            f'функция: {func.__name__}, аргументы функции: {args},'
-            f' результат функции: {result} ')
-        return result
+        try:
+            result = func(*args, **kwargs)
+            logger.info(
+                f'функция: {func.__name__}, аргументы функции: {args},'
+                f' результат функции: {result} ')
+            return result
+        except Exception as e:
+            logger.error(f'функция: {func.__name__} вызвала ошибку: {e}')
 
     return wrapper
 
 
-@decor
+@decor_logger
 def power(x, y):
     return x ** y
 
